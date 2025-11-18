243ca127aeb4b159139f469a865d189df7b8a90a
MapleQiAN <serendylin@outlook.com>
2025-11-18 14:07:44 +0800

    Revert "Update dependencies and integrate Vue DevTools plugin"
    This reverts commit f55720e013700b10caa8a8c1d4a2b7f56cbad25e.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
