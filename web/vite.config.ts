f55720e013700b10caa8a8c1d4a2b7f56cbad25e
MapleQiAN <serendylin@outlook.com>
2025-11-18 13:52:13 +0800

    Update dependencies and integrate Vue DevTools plugin
    - Reverted the naive-ui and vue-echarts versions in package.json to maintain compatibility.
    - Added vite-plugin-vue-devtools to enhance development experience with Vue applications.
    - Updated pnpm-lock.yaml to reflect changes in dependencies and ensure consistent package resolution.
    - Modified vite.config.ts to include the Vue DevTools plugin in the Vite configuration, improving debugging capabilities during development.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
